6bf76566011bffade081797159ed23bc42e618e6
guilherme-fcm <guilherme_fcm@hotmail.com>
2022-09-20 16:45:08 -0300

    Add create method on person controller

--- server/controllers/PersonController.js
+++ server/controllers/PersonController.js
@@ -1,9 +1,17 @@
 const Person = require('../models/Person')
 
 class PersonController {
-    async index(request, response) {
-        const persons = await Person.findAll()
-        return response.json(persons)
+    async index(request, response){
+        const people = await Person.findAll()
+        return response.json(people)
+    }
+
+    async create(request, response){
+        const { firstName, lastName, email, phone, gender, about, address, image } = request.body;
+        const person = await Person.create(
+            { firstName, lastName, email, phone, gender, about, address, image }
+        )
+        return response.json(person)
     }
 }
 
