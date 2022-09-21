e252351916808df4b03a5d182c47fc196eed9f85
guilherme-fcm <guilherme_fcm@hotmail.com>
2022-09-21 10:33:00 -0300

    Modify server port

--- server/controllers/PersonController.js
+++ server/controllers/PersonController.js
@@ -1,4 +1,3 @@
-const { where } = require('sequelize')
 const Person = require('../models/Person')
 
 class PersonController {
--- server/server.js
+++ server/server.js
@@ -5,4 +5,4 @@ const app = express()
 app.use(express.json())
 app.use(routes)
 
-app.listen(3000, () => { console.log('Server started on port 3000.') })
+app.listen(3001, () => { console.log('Server started on port 3001.') })
