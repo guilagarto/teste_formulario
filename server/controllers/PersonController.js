06b23a87250425dbc3515b7a737ab6d149c028e8
guilherme-fcm <guilherme_fcm@hotmail.com>
2022-09-26 10:57:47 -0300

    Handle errors

--- server/controllers/PersonController.js
+++ server/controllers/PersonController.js
@@ -9,31 +9,44 @@ class PersonController {
     async find(request, response){
         const { id } = request.params
         const person = await Person.findOne({ where: { id } })
+        if (! person) return response.status(404).json({ message: 'Person not found.' })
         return response.json(person)
     }
 
     async create(request, response){
         const { firstName, lastName, email, phone, gender, about, address, image } = request.body;
-        const person = await Person.create(
-            { firstName, lastName, email, phone, gender, about, address, image }
-        )
-        return response.json(person)
+        try {
+            const person = await Person.create(
+                { firstName, lastName, email, phone, gender, about, address, image }
+            )
+            return response.json(person)
+        } catch (error){
+            return response.status(400).json({
+                message: 'Unable to complete registration.',
+                error
+            })
+        }
     }
 
     async destroy(request, response){
         const { id } = request.params
-        const person = await Person.destroy({ where: { id } })
-        return response.status(204).json()
+        const result = await Person.destroy({ where: { id } })
+
+        if (result === 0) return response.status(404).json({ message: 'Person not found.' })
+        return response.status(204).send()
     }
 
     async update(request, response){
         const { firstName, lastName, email, phone, gender, about, address, image } = request.body;
         const { id } = request.params
-        const person = await Person.update(
+
+        const [ result ] = await Person.update(
             { firstName, lastName, email, phone, gender, about, address, image },
             { where: { id } }
         )
-        return response.status(204).json()
+
+        if (result === 0) return response.status(404).json({ message: 'Person not found.' })
+        return response.status(204).send()
     }
 }
 
